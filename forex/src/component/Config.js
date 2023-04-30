eef5db38d7338a81791c81c3625d3401b8d74567
Shalmalinighot <shalmali.nighot@gmail.com>
2023-04-30 12:12:19 +0530

    Initial commit

---
+++ forex/src/component/Config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+
+    //currencyConverterUrl:"http://localhost:8082",
+    addTransactionUrl:"http://localhost:8081/transaction/save",
+    saveUserBankDetailsUrl:"http://localhost:8081/UserBankDetails/save",
+    currencyConverterUrl:"http://localhost:8081",
+    updateRateUrl:"http://localhost:8081/rate/",
+    updateUrlSave:"http://localhost:8081/admin/exchangeRate/update",
+    getRateByDate:" http://localhost:8081/exchange-rates/" 
+    }
